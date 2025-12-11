c19317b10f78cda97d27f3572085b3d0c4e03ca9
Art-Xz <arturgaudie08@gmail.com>
2025-12-11 05:28:25 -0300

    beecrowd 3048

---
+++ sequencia_secreta.py
@@ -0,0 +1,11 @@
+N = int(input())
+counter = 1
+test = int(input())
+for i in range(N-1):
+    C = int(input())
+    if C != test:
+        counter +=1
+    test = C
+print("-"*30)
+print(counter)
+

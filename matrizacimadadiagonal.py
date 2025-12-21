08859219f8a49a0b30b631c052b114bbb97dd9ba
Art-Xz <arturgaudie08@gmail.com>
2025-12-21 05:37:15 -0300

    beecrowd 1183|oops

--- matrizacimadadiagonal.py
+++ matrizacimadadiagonal.py
@@ -1,21 +1,22 @@
 matriz = []
-num = -1
 soma = 0.0
 lista = []
+x = input()
 for i in range(12):
     linha = []
     for j in range(12):
-        num +=1
-        #num = float(input())
+        
+        num = float(input())
         linha.append(num)
     matriz.append(linha)
 
-
-for a in range(12):
-    lista.append(matriz[a][a])
-print(lista)
 for b in range(12):
     for c in range(12):
         if c>b:
             soma+=matriz[b][c]
-print(soma)
+
+if x == "S":
+    print(f"{soma:.1f}")
+else:
+    media = soma/66
+    print(f"{media:.1f}")

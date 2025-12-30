bb5956133cf8a5b88d19e3476dae0b3fdc0c71c6
Art-Xz <arturgaudie08@gmail.com>
2025-12-30 05:05:59 -0300

    beecrowd 1188

---
+++ somadaareainferior.py
@@ -0,0 +1,22 @@
+matriz = []
+soma = 0.0
+quest = input()
+counter = 0
+for i in range(12):
+    linha = []
+    for j in range(12):
+        x = float(input())
+        linha.append(x)
+    matriz.append(linha)
+
+for a in range(12):
+    for b in range(12):
+        if b<a and a+b>11:
+            counter +=1
+            soma += matriz[a][b]
+
+if quest == "S":
+    print(f"{soma:.1f}")
+else:
+    media = soma/counter
+    print(f"{media:.1f}")

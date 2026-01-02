b8191de587439b5d1ae0caf560fc5a81232ae263
Art-Xz <arturgaudie08@gmail.com>
2026-01-02 06:42:40 -0300

    ?

--- vaitercopa.py
+++ vaitercopa.py
@@ -1,6 +1,9 @@
-n = int(input())
-
-if n > 0:
-    print("vai ter duas!")
-else:
-    print("vai ter copa!")
+while True:
+    try:
+        n = int(input())
+        if n > 0:
+            print("vai ter duas!")
+        else:
+            print("vai ter copa!")
+    except EOFError:
+        break

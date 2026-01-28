b4fbf4df799917b6d8359faa7554956ee707d288
Art-Xz <arturgaudie08@gmail.com>
2026-01-28 05:07:05 -0300

    beecrowd 1962

---
+++ hamuitotempo.py
@@ -0,0 +1,12 @@
+n = int(input())
+ano = 2015
+for i in range(n):
+    t = int(input())
+    passar = (ano) - t
+    if passar<0:
+        print(f"{abs(passar-1)} A.C")
+    if passar==0:
+        print(f"{1} D.C")
+    if passar>0:
+        print(f"{passar} D.C")
+    

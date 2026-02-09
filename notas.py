b96686a35e8b7c50be886c456cefbc5aa871593d
Art-Xz <arturgaudie08@gmail.com>
2026-02-09 04:53:08 -0300

    beecrowd 2344

---
+++ notas.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+if n < 1:
+    print("E")
+if n > 1 and n <=35:
+    print("D")
+if n > 35 and n <= 60:
+    print("C")
+if n > 60 and n <= 85:
+    print("B")
+if n > 85:
+    print("A")

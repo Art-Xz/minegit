a79d52d5c73d7a67409dbd6a0447d784cd4b0083
Art-Xz <arturgaudie08@gmail.com>
2026-02-14 06:22:32 -0300

    beecrowd 1001 C++

---
+++ numX.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int aa;
+    cin>>aa;
+    int be;
+    cin>>be;
+    
+    int sum = aa+be;
+    cout<<"X = "<<sum<<"\n";
+ 
+    return 0;
+}
